1ae148fd3921fc1480d79bac1bf3d5e443771913
lbidulka <lukebidulka@gmail.com>
2024-02-09 10:00:29 -0800

    add leave-one-out eval

--- data/timeseries/data_timeseries.py
+++ data/timeseries/data_timeseries.py
@@ -6,7 +6,6 @@ class data_timeseries():
         self.dataset_path = dataset_path
         self.x, self.y, self.subj_ids, self.handednesses = self.load_dataset_file(dataset_path)
         
-
     def load_dataset_file(self, file_path):
         data = np.load(file_path)
         x = data['samples']
@@ -15,5 +14,14 @@ class data_timeseries():
         handednesses = data['handednesses']
         return x, y, subj_ids, handednesses
     
+    def get_subj_data(self, subj_id):
+        '''
+        Get all samples for specified subject
+        '''
+        subj_idxs = np.where(self.subj_ids == subj_id)[0]
 
-
+        # get data for this subject
+        subj_data = self.x[subj_idxs]
+        subj_labels = self.y[subj_idxs]
+        return [subj_data, subj_labels]
+    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import numpy as np
 
 from models.dsp_updrs import UPDRS_DSP
 import data.timeseries.data_timeseries as data_timeseries
@@ -14,6 +15,39 @@ def parse_args():
     return args
 
 
+def leave_one_out_eval(model, data):
+    '''
+    leave-one-out train/evaluation over all samples. Excluding 1 subjects samples at a time.
+    '''
+    eval_preds, eval_targets = [], []
+    subj_ids = np.unique(data.subj_ids)
+    for subj_id in subj_ids:
+        eval_subj_data = data.get_subj_data(subj_id)
+        
+        # train over all samples except this subject's samples
+        # NO TRAIN FOR NOW
+
+        # eval on excluded subject's data
+        eval_pred = model(eval_subj_data[0])
+        eval_target = eval_subj_data[1]
+
+        eval_pred, eval_target = data_utils.remove_unlabeled(eval_pred, eval_target)    # don't include samples with either label == -1
+        eval_target = eval_target[:,1]  # TEMP: only using 2nd label for now
+
+        eval_preds.append(eval_pred)
+        eval_targets.append(eval_target)
+        
+    eval_preds = np.hstack(eval_preds)
+    eval_targets = np.hstack(eval_targets)
+    metrics = eval_utils.get_metrics(eval_preds, eval_targets)
+    print('\n--- Metrics: ---')
+    for metric, value in metrics.items():
+        if metric == 'conf_mat':
+            print(f'{metric}: \n{value}')
+        else:
+            print(f'{metric}: {value:.3f}')
+
+
 if __name__ == '__main__':
     args = parse_args()
 
@@ -23,20 +57,9 @@ if __name__ == '__main__':
 
     # Define the model and data
     data = data_timeseries.data_timeseries(data_path)
-    updrs_dsp = UPDRS_DSP()
+    model = UPDRS_DSP()
 
-    # evaluate over whole dataset
-    preds = updrs_dsp(data.x)
-    targets = data.y
+    # Evaluate the model
+    leave_one_out_eval(model, data)
 
-    preds, targets = data_utils.remove_unlabeled(preds, targets)    # don't include samples with either label == -1
-    targets = targets[:,1]
-
-    metrics = eval_utils.get_metrics(preds, targets)
-
-    print('\n--- Metrics: ---')
-    for metric, value in metrics.items():
-        if metric == 'conf_mat':
-            print(f'{metric}: \n{value}')
-        else:
-            print(f'{metric}: {value:.3f}')
+    
